1db4115b16f9edbe710d2082d2f337eea754a6e3
horlumiday <oluwadareysamuel@yahoo.com>
2019-07-02 12:17:41 +0100

    Layout component imported into PhotoGallery component

--- src/container/PhotoGallery/PhotoGallery.js
+++ src/container/PhotoGallery/PhotoGallery.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Layout from '../../components/Layout/Layout';
 
 class PhotoSplash extends Component {
 
 	render() {
 		return (
-	);
-	};
+			<Layout>
+
+			</Layout>
+		)
+	}
 }
 
 export default PhotoSplash;

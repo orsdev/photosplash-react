1be2cbcffed3a1cae7fcbf568e8643cc93f3cd83
horlumiday <oluwadareysamuel@yahoo.com>
2019-07-02 12:21:23 +0100

    InputField component imported into PhotoGallery component

--- src/container/PhotoGallery/PhotoGallery.js
+++ src/container/PhotoGallery/PhotoGallery.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout/Layout';
+import InputField from '../../components/InputField/InputField';
 
 class PhotoSplash extends Component {
 
 	render() {
 		return (
 			<Layout>
-
+				<InputField 
+					keyCodeSearch={this.KeyCodeSearch} buttonSearch={this.ButtonSearch} />
 			</Layout>
 		)
 	}

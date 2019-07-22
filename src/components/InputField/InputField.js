f3db2e130491e68a14486b7b1ea8400ca037206b
horlumiday <oluwadareysamuel@yahoo.com>
2019-07-22 04:38:19 +0100

    InputField components: input element class removed, button element class changed to search , made some adjustment to styling

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -4,8 +4,8 @@ const inputField = (props) => {
   return (
     <div className='form-container'>
       <div className='form-group'>
-        <input onKeyUp = {props.keyCodeSearch} id = 'search' type = 'search' placeholder = 'Search Here...' className = 'form-control' />
-        <button onClick = {props.buttonSearch} className = 'button'><i className = 'fa fa-search'></i></button>
+        <input onKeyUp = {props.keyCodeSearch} id = 'search' type = 'search' placeholder = 'Search Here...'  />
+        <button onClick = {props.buttonSearch} className = 'search'><i className = 'fa fa-search'></i></button>
       </div>
     </div>
   )

f17dcaf0346b19ff9efc7e7ed52d4a99e8323d46
horlumiday <oluwadareysamuel@yahoo.com>
2019-07-22 04:59:50 +0100

    InputField components: imported Button component , styled Button component in InputField component

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Button from '../../UI/Button/Button';
 
 const inputField = (props) => {
   return (
     <div className='form-container'>
       <div className='form-group'>
-        <input onKeyUp = {props.keyCodeSearch} id = 'search' type = 'search' placeholder = 'Search Here...'  />
-        <button onClick = {props.buttonSearch} className = 'search'><i className = 'fa fa-search'></i></button>
+        <input onKeyUp={props.keyCodeSearch} id='search' type='search' placeholder='Search Here...' />
+        <button onClick={props.buttonSearch} className='search'><i className='fa fa-search'></i></button>
+      </div>
+      <div className='btn-group'>
+        <Button classe='btn btn-outline-danger' name='MEN' />
+        <Button classe='btn btn-outline-warning' name='WOMEN' />
+        <Button classe='btn btn-outline-dark' name='NATURE' />
+        <Button classe='btn btn-outline-secondary' name='TECHNOLOGY' />
+        <Button classe='btn btn-outline-primary' name='CHILDREN' />
+        <Button classe='btn btn-outline-success' name='ANIMALS' />
       </div>
     </div>
   )

ef7eff1ec781a13037a9a3b5d1a806a285f1a959
horlumiday <oluwadareysamuel@yahoo.com>
2019-07-29 07:56:38 +0100

    Button , Modal , Spinner component: indention spaces reduced

--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -2,22 +2,22 @@ import React, { memo } from 'react';
 
 const Modal = (props) => {
 
-    let message = null;
+  let message = null;
 
-    if (props.error) {
-        message = (
-            <div className='modal-pop' onClick= {props.toggle}>
-                <div className='modal-pop-dialog'>
-                    <div className='message'>
-                    <p> {props.error} </p>
-                    <button onClick={props.toggle} className='close-btn'>x</button>
-                    </div>
-                </div>
-            </div>
-    
-        )
-    }
-    return message;
+  if (props.error) {
+    message = (
+      <div className='modal-pop' onClick={props.toggle}>
+        <div className='modal-pop-dialog'>
+          <div className='message'>
+            <p> {props.error} </p>
+            <button onClick={props.toggle} className='close-btn'>x</button>
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+  return message;
 }
 
-export default memo( Modal );
+export default memo(Modal);
--- src/UI/Spinner/Spinner.js
+++ src/UI/Spinner/Spinner.js
@@ -1,20 +1,20 @@
 import React from 'react'
 
 const Spinner = (props) => {
-    return (
-        <div className='spinner_container'>
-            <div className='lds-roller'>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-        </div>
-    );
+  return (
+    <div className='spinner_container'>
+      <div className='lds-roller'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default Spinner;

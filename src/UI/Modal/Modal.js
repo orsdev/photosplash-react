fff3f8ffdba6dbcb44ae16c3c91af258dbb00612
horlumiday <oluwadareysamuel@yahoo.com>
2019-08-13 06:55:01 +0100

    Modal UI component: deleted some unused code(deleted outer div)

--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -6,15 +6,12 @@ const Modal = (props) => {
 
   if (props.error) {
     message = (
-      <div className='modal-pop' onClick={props.toggle}>
-        <div className='modal-pop-dialog'>
+        <div className='modal-pop-dialog' onClick={props.toggle}>
           <div className='message'>
             <p> {props.error} </p>
             <button onClick={props.toggle} className='close-btn'>x</button>
           </div>
         </div>
-      </div>
-
     )
   }
   return message;

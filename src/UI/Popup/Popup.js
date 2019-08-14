77cbb415f7cc3634610523530ad86b6169ab7a1f
horlumiday <oluwadareysamuel@yahoo.com>
2019-08-14 06:41:14 +0100

    New component added: Popup(UI) and _Popup.scss

---
+++ src/UI/Popup/Popup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const popup =(props) => {
+   return (
+      <div className = 'popup'>
+         <span onClick = {props.close} className = 'close-btn'>X</span>
+         <img src = {props.src}  alt="popup" className = 'img-popup'/>
+      </div>
+   );
+};
+
+export default popup;

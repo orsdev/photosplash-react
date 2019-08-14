c7588d24f54785afb298d72b0f1f7a39312f0ec4
horlumiday <oluwadareysamuel@yahoo.com>
2019-08-14 06:44:08 +0100

    Gallery.js modified: click event added to img

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -4,7 +4,7 @@ const Gallery = (props) => {
 if(props.searcedImages !== 0) {
  return (
     <div className = { `gallery__item--${props.id}`}>
-      <img src={props.pic} alt={props.alt} />
+      <img src={props.pic} alt={props.alt} onClick={props.getSrc} />
     </div>
  );
 }

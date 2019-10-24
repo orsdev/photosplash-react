24f04708780cf35c6bef26c5032380829d83b0c3
horlumiday <oluwadareysamuel@yahoo.com>
2019-10-24 11:05:27 +0100

    modified some files

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,5 +1,3 @@
-export const PREV_PAGE = 'PREVIOUS_PAGE';
-export const NEXT_PAGE = 'NEXT_PAGE';
 export const AJAX_REQUEST = 'AJAX_REQUEST';
 export const AJAX_FAIL = 'AJAX_FAIL';
 export const AJAX_SUCCESS = 'AJAX_SUCCESS';
